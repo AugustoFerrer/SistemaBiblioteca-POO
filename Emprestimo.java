db11652e2b4c4c2f6be27320cf1a8321946cfe88
AugustoFerrer <gutoprogramas05@gmail.com>
2025-10-17 12:27:16 -0300

    entendendo mais a logica do emprestimo

--- Emprestimo.java
+++ Emprestimo.java
@@ -5,15 +5,59 @@ public class Emprestimo {
     private Usuario usuario;
     private Livro livro;
     private LocalDate dataIni;
+    private LocalDate dataEntregue;
     private LocalDate dataFim;
-    private boolean ativo;
+    // private int diasAtrasado;
+    // private boolean atrasado;        ESSES ATRIBUTOS MUDAM COM O TEMPO
+    // private boolean ativo;
 
     public Emprestimo(Usuario usuario, Livro livro){
         this.usuario = usuario;
         this.livro = livro;
         this.dataIni = LocalDate.now();
+        this.dataEntregue = null;
         this.dataFim = dataIni.plusDays(14);
-        this.ativo = true;
+        // this.diasAtrasado = 0;
+        // this.atrasado = false;
+        // this.ativo = true;
     }
 
+   
+
+    //nao sei outras funcoes de emprestimo
+    
+    
+    //GETTERS
+
+    public Usuario getUsuario(){
+        return this.usuario;
+    }
+
+    public Livro getLivro(){
+        return this.livro;
+    }
+
+    public LocalDate getDataIni(){
+        return this.dataIni;
+    }
+
+    public LocalDate getDataEntregue(){
+        return this.dataEntregue;
+    }
+
+    public LocalDate getDataFim(){
+        return this.dataFim;
+    }
+
+    // public boolean isAtrasado(){
+    //     return this.atrasado;
+    // }
+
+    // public int getDiasAtrasado(){
+    //     return this.diasAtrasado;
+    // }
+    
+    // public boolean isAtivo(){
+    //         return this.ativo;
+    //     }
 }
--- Usuario.java
+++ Usuario.java
@@ -3,17 +3,24 @@ import java.util.List;
 
 public class Usuario {
     
-    private String nome;
-    private int matricula;
-    private List<Emprestimo> emprestimos = new ArrayList<>();
-    private int limiteEmprestimos = 3; 
-
-    //CONSTRUTOR
+    private final String nome;
+    private final int matricula;
+    private final int limiteEmprestimos; 
+    private List<Emprestimo> emprestimos;
+    
+    //CONSTRUTOR FOCANDO AS INICIALIZAÇÕES NELE
     public Usuario(String nome, int matricula){
         this.nome = nome;
         this.matricula = matricula;
+        this.limiteEmprestimos = 3;
+        this.emprestimos = new ArrayList<>();
     }
 
-    
+    // public ... PegarEmprestado(Livro livro){
+
+
+    // }
+
+
 
 }
